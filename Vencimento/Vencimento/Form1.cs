707e16b3f7d3af52e9348a7c7511f46191ffab1b
HaroIvanP <64097121+HaroIvanP@users.noreply.github.com>
2020-04-21 19:18:05 +0100

    Add files via upload

---
+++ Vencimento/Vencimento/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vencimento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Tem a certeza?", "Question", MessageBoxButtons.OKCancel, MessageBoxIcon.None, MessageBoxDefaultButton.Button1) == DialogResult.OK)
+            {
+                Application.Exit(); // or this.Close();
+            }
+        }
+            private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
